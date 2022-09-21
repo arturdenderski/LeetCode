b9e967304e4b26ae6e47d43e855383f42d015ecc
artur1101 <artur.denderski@gmail.com>
2022-09-21 19:35:48 +0200

    Two Sum

--- Easy/Two-Sum.cpp
+++ Easy/Two-Sum.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+
         unordered_map<int,int> map;
+
         for(int i = 0; i < nums.size(); i++)
         {
             if(map.find(target-nums[i])!=map.end())
